ce6f291bbcbad2d3d3fdbac7e0d58d0079d14436
Jhoan DIja <albertdija@gmail.com>
2024-04-04 21:48:52 -0400

    PRACTICA 2 y 3 REALIZADAS

--- P2/cnn.py
+++ P2/cnn.py
@@ -1,23 +1,24 @@
 import numpy as np
 
-# Define the Euclidean distance function
+# Distancia euclideana
 def euclidean_distance(x1, x2):
     return np.sqrt(np.sum((x1 - x2) ** 2))
 
-# Implement the Condensed Nearest Neighbors algorithm
-def condensed_nearest_neighbors(X, y):
-    S = [X[0]]  # Initialize the subset with the first instance
-    S_labels = [y[0]]
+# Condensed nearest neighboor
+def condensed_nearest_neighbors(Z, y):
+    S = [Z[0]]  # Inicializando el subespacio S del espacio Z
+    S_labels = [y[0]] #Clases del subespacio S
     
-    for i in range(1, len(X)):
-        misclassified = True
+    #Aqui chequeamos si ya existe un punto en S el cual ya exista en el conjunto de datos Z
+    for i in range(1, len(Z)):
+        no_existe = True
         for j in range(len(S)):
-            if S_labels[j] == y[i] and euclidean_distance(S[j], X[i]) == 0:
-                misclassified = False
+            if S_labels[j] == y[i] and euclidean_distance(S[j], Z[i]) == 0:
+                no_existe = False
                 break
         
-        if misclassified:
-            S.append(X[i])
+        if no_existe:
+            S.append(Z[i])
             S_labels.append(y[i])
     
     return np.array(S), np.array(S_labels)
